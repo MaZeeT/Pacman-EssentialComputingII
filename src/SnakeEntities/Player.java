d66003d0094a87d61cb0896d3decc46ae2b9ee72
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-02-20 11:24:08 +0100

    Update Player

---
+++ src/SnakeEntities/Player.java
@@ -0,0 +1,14 @@
+package SnakeEntities;
+
+import SnakeComponents.Position;
+
+public class Player extends GameObject {
+
+    public Player(Position position) {
+        this.position = position;
+    }
+
+    public void update() {
+
+    }
+}
