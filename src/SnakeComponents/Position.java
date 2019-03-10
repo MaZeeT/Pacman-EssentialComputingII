158739ae4aff68c9c734b61a254cb6ce199223d4
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-03-10 22:38:42 +0100

    position: test + compare method added

--- src/SnakeComponents/Position.java
+++ src/SnakeComponents/Position.java
@@ -1,6 +1,6 @@
 package SnakeComponents;
 
-public class Position {
+public class Position implements Comparable<Position> {
 
     private int x;
     private int y;
@@ -16,5 +16,30 @@ public class Position {
 
     public int getY(){
         return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Position pos) {
+        int rtn = 0;
+        if (x == pos.getX() && y == pos.getY()){
+            return 0;
+        }
+        if (x < pos.getX()) rtn = -1;
+        if (x > pos.getX()) rtn = +1;
+
+        if(rtn == 0) {
+            if (y < pos.getY()) rtn = -1;
+            if (y > pos.getY()) rtn = +1;
+        }
+
+        return rtn;
     }
 }
---
+++ test/SnakeComponents/PositionTest.java
@@ -0,0 +1,106 @@
+package SnakeComponents;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PositionTest {
+
+    private int defX;
+    private int defY;
+    private Position pos;
+
+    @Before
+    public void setUp(){
+        defX = 2;
+        defY = 4;
+        pos = new Position(defX,defY);
+    }
+
+    @Test
+    public void getX() {
+        int expected = defX;
+        int actual = pos.getX();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getY() {
+        int expected = defY;
+        int actual = pos.getY();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setX() {
+        int expected = 2;
+        pos.setX(expected);
+        int actual = pos.getX();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setY() {
+        int expected = 5;
+        pos.setY(expected);
+        int actual = pos.getY();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareEqual() {
+        Position newPos = new Position(defX,defY);
+        int expected = pos.compareTo(newPos);
+        int actual = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareToHigherX() {
+        Position newPos = new Position(defX+1,defY);
+        int expected = pos.compareTo(newPos);
+        int actual = -1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareToLowerX() {
+        Position newPos = new Position(defX-1,defY);
+        int expected = pos.compareTo(newPos);
+        int actual = +1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareToHigherY() {
+        Position newPos = new Position(defX,defY+1);
+        int expected = pos.compareTo(newPos);
+        int actual = -1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareToLowerY() {
+        Position newPos = new Position(defX,defY-1);
+        int expected = pos.compareTo(newPos);
+        int actual = +1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareToHigherXY() {
+        Position newPos = new Position(defX+1,defY+1);
+        int expected = pos.compareTo(newPos);
+        int actual = -1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareToLowerXY() {
+        Position newPos = new Position(defX-1,defY-1);
+        int expected = pos.compareTo(newPos);
+        int actual = +1;
+        assertEquals(expected, actual);
+    }
+}
