f7be6039c35539b39de1ca6ffa15fcfb87266732
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-03-27 11:05:26 +0100

    add javaDoc

--- src/SnakeLogic/GameManager.java
+++ src/SnakeLogic/GameManager.java
@@ -7,10 +7,16 @@ import SnakeLogic.Crawler.Crawler;
 import SnakeLogic.Crawler.MoveClockWise;
 import SnakeMaze.IMaze;
 
-
 import java.util.*;
 
-//TODO write javaDoc
+/**
+ * The purpose of this class is, to combine most of the components needed for the logic to work.
+ * All the different types of {@link GameObject}s are access here.
+ * The {@link Crawler} is combined with the {@link IMaze} and {@link IDataStructure}.
+ * It is also possible to disable the {@link Crawler} to control with {@link SnakeUserControl.UserControl} instead.
+ *
+ * @author MaZeeT
+ */
 public class GameManager {
 
     private List<GameObject> walls;
@@ -23,6 +29,12 @@ public class GameManager {
 
     private boolean crawl = false;
 
+    /**
+     * Constructor of this class. It takes a {@link IMaze} and a {@link IDataStructure} as input.
+     *
+     * @param maze          The {@link IMaze} is used to access all the different {@link GameObject}s in the {@link IMaze}.
+     * @param dataStructure The {@link IDataStructure} is used to store each possible movement, and returns the next move.
+     */
     public GameManager(IMaze maze, IDataStructure dataStructure) {
         walls = maze.getWalls();
 
@@ -34,25 +46,38 @@ public class GameManager {
         crawler = new Crawler(maze, dataStructure);
     }
 
-    // update loop to make the game run
+    /**
+     * The update method, which is called each update tick.
+     */
     public void update() {
         movement.move(direction);
         direction = movement.getDirection();
-
         if (crawl) crawler.update();
     }
 
-    // get and set
+    /**
+     * Set the direction.
+     *
+     * @param direction The direction as a char.
+     */
     public void setDirection(char direction) {
         this.direction = direction;
-        System.out.println(direction);
     }
 
+    /**
+     * Get the direction as char.
+     *
+     * @return Returns the direction as a char.
+     */
     char getDirection() {
         return this.direction;
     }
 
-    //export gameObjects for GUI
+    /**
+     * Combines the different {@link GameObject}s in a list and returns this list.
+     *
+     * @return Returns a list of all the {@link GameObject}s combined.
+     */
     private List<GameObject> combineGameObjects() {
         List<GameObject> gameObjects = new ArrayList<>();
         gameObjects.addAll(wayPoints);
@@ -62,6 +87,11 @@ public class GameManager {
         return gameObjects;
     }
 
+    /**
+     * Get a list of {@link GameObject}s.
+     *
+     * @return Returns a list of {@link GameObject}s.
+     */
     public List<GameObject> getGameObjects() {
         return combineGameObjects();
     }
