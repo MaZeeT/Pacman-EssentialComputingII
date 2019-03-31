fcbe1b331a81704a0d7fc5759acb9b061a762f40
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-03-31 16:27:33 +0200

    added test to MazeWithExit

--- test/Maze/MazeWithExitTest.java
+++ test/Maze/MazeWithExitTest.java
@@ -1,19 +1,48 @@
 package Maze;
 
+import Components.Position;
+import Entities.GameObject;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class MazeWithExitTest {
-//TODO no tests in this test class... Doh..
-    private Maze maze;
+    private Position playerPosition;
+    private Position wayPointPosition;
+    private List<GameObject> walls;
+
     @Before
     public void setUp(){
-        maze = new MazeWithExit();
+       IMaze  maze = new MazeWithExit();
+       playerPosition = maze.getPlayer().getPosition();
+       wayPointPosition = maze.getWayPoint().getPosition();
+       walls = maze.getWalls();
     }
 
     @Test
-    public void generateMaze() {
-       // maze.generateMaze();
+    public void checkWalls(){
+        int expected = 27;
+        int actual = walls.size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkWayPoint(){
+        Position position = new Position(1,5);
+        int expected = 0;
+        int actual = wayPointPosition.compareTo(position);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkPlayer(){
+        Position position = new Position(1,1);
+        int expected = 0;
+        int actual = playerPosition.compareTo(position);
+        assertEquals(expected, actual);
     }
 
 }
