a449d722dab8d6a82896fbc271ffd93ca1b4c91e
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-04-03 00:57:31 +0200

    Added PacMan maze

---
+++ test/Maze/PacManTest.java
@@ -0,0 +1,49 @@
+package Maze;
+
+import Components.Position;
+import Entities.GameObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PacManTest {
+
+    private Position playerPosition;
+    private Position wayPointPosition;
+    private List<GameObject> walls;
+
+    @Before
+    public void setUp(){
+        IMaze maze = new PacMan();
+        this.playerPosition = maze.getPlayer().getPosition();
+        this.wayPointPosition = maze.getWayPoint().getPosition();
+        this.walls = maze.getWalls();
+    }
+
+    @Test
+    public void checkWalls(){
+        int expected = 672;
+        int actual = walls.size();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkWayPoint(){
+        Position position = new Position(15,14);
+        int expected = 0;
+        int actual = wayPointPosition.compareTo(position);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkPlayer(){
+        Position position = new Position(15,24);
+        int expected = 0;
+        int actual = playerPosition.compareTo(position);
+        assertEquals(expected, actual);
+    }
+
+}
