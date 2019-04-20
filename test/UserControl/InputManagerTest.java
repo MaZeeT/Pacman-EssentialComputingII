058ba2b7d5e21882c2d651e282c693251ffd227b
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-04-20 22:20:07 +0200

    added a few tests for the InputManager Class

--- test/UserControl/InputManagerTest.java
+++ test/UserControl/InputManagerTest.java
@@ -1,21 +1,53 @@
 package UserControl;
 
+import DataStructures.DepthFirst;
+import Maze.IMaze;
+import Maze.PacMan;
+import Movement.Crawler;
+import Movement.IMover;
+import Movement.IMoverControlled;
+import Movement.MoveClockWise;
+import javafx.scene.input.KeyCode;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class InputManagerTest {
+    private KeyCode keyCode;
+    private IMover mover;
+    private IMoverControlled moverControlled;
+    private UserInput userInput;
+    private InputManager inputManager;
 
     @Before
-    public void setUp(){
+    public void setUp() {
+        IMaze maze = new PacMan();
+        keyCode = KeyCode.W;
+        mover = new Crawler(maze, maze.getPlayer(), new DepthFirst());
+        moverControlled = new MoveClockWise(maze, maze.getPlayer());
+        userInput = new PlayerControl();
     }
 
     @Test
-    public void directionW() {
+    public void controlledUpdateKeycodeW() {
+        inputManager = new InputManager(moverControlled, userInput);
+        inputManager.update(keyCode);
 
         char actual = 'w';
-        char expected = 's';
+        char expected = moverControlled.getDirection();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void uncontrolledUpdateKeycodeW() {
+        inputManager = new InputManager(mover, userInput);
+        inputManager.update(keyCode);
+
+        // "\0" is the "null" char
+        char actual = '\0';
+        char expected = moverControlled.getDirection();
+        assertEquals(expected, actual);
+    }
+
 }
