3423da95ad9c4a44bdaf5c3befd7fa91be5eb8ea
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-04-21 16:17:42 +0200

    worked on todo's

--- src/WorkInProgress/Tree.java
+++ src/WorkInProgress/Tree.java
@@ -6,6 +6,7 @@ import WorkInProgress.Nodes.SimpleNode;
 //TODO implement a tree
 //TODO Make a shortest route algorithm based on a tree.
 public class Tree<T> {
+
     //NodeX<T> root = new NodeX<>();
     //SimpleNode<T> root;
     private Node<T> root;
---
+++ src/WorkInProgress/TreeClimber.java
@@ -0,0 +1,20 @@
+package WorkInProgress;
+
+import Movement.IMover;
+import Movement.IMoverControlled;
+
+public class TreeClimber implements IMover {
+
+    public TreeClimber(){
+        // new tree
+        // exploreTree()
+        // get list or queue
+    }
+
+    @Override
+    public void move() {
+        //set position to shortestPathList.next()
+
+    }
+
+}
